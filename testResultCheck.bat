bc5d564fb360cea6c458ec1ffa6e241b7eb6a0ba
Dmitry Kudelko <dkudelko@scanex.ru>
2013-02-14 14:40:05 +0400

    правка

--- testResultCheck.bat
+++ testResultCheck.bat
@@ -1,46 +1,14 @@
 @echo off
+rem remember the directory path to this bat file
+set dirPath=%~dp0
 
-rem %1 D:\TeamCityTestData\
-rem %2 D:\TeamCity\buildAgent\work\160ea70fbfb7de1f
+rem need to reverse windows names to posix names by changing \ to /
+set dirPath=%dirPath:\=/%
+rem remove blank at end of string
+set dirPath=%dirPath:~0,-1%
 
-:test0
-echo ##teamcity[testStarted name='.tiles exist']
-set filename="%2\testOutput\scn_120719_Vrangel_island_SWA.tiles"
+rem - Customize for your installation, for instance you might want to add default parameters like the following:
+java -jar "%dirPath%"/lib/jira-cli-3.1.0.jar --server http://5.9.141.80:8080 --user dkudelko --password JR4545hj %*
 
-if exist %filename% goto nexttest0
-:nexttest0
-echo ##teamcity[testFinished name='.tiles exist']
-goto test1
-:errortest0
-echo  ##teamcity[testFailed name='.tiles exist']
-
-
-:test1
-echo ##teamcity[testStarted name='Check .tiles file size']
-set tileFileSize=0
-for %%A in (%filename%) do set tileFileSize= %%~zA 
-echo %tileFileSize%
-
-if %tileFileSize% == 3978095 goto nexttest1
-:errortest1
-echo ##teamcity[testFailed name='Check .tiles file size']
-goto test2
-
-:nexttest1
-echo ##teamcity[testFinished name='Check .tiles file size']
-
-
-
-:test2
-echo ##teamcity[testStarted name='Compare Files']
-set fileSource=%1\TilingTools\5_16_8.jpg
-set fileGenerated=%2\testOutput\copy1\5\16\5_16_8.jpg
-fc %fileSource% %fileGenerated% > nul
-if errorlevel 1 goto errortest2
-:nexttest2
-echo ##teamcity[testFinished name='Compare Files']
-goto test3
-:errortest2
-echo ##teamcity[testFailed name='Compare Files']
-
-:test3
+rem Exit with the correct error level.
+EXIT /B %ERRORLEVEL%
