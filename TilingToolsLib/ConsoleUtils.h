d3b260b48eec97f4a48ee8c3f4dc2a0322d6605a
Michigan <mike@scanex.ru>
2016-05-27 11:03:46 +0300

    test

--- TilingToolsLib/ConsoleUtils.h
+++ TilingToolsLib/ConsoleUtils.h
@@ -5,14 +5,12 @@
 
 namespace gmx
 {
-
 	
 void	SetEnvironmentVariables (string gdal_path);
 bool	LoadGDAL (int argc, string argv[]);
 
 bool	LoadGDALDLLs (string gdal_path);
 string	ReadGDALPathFromConfigFile (string config_file_path);
-//string	ReadGDALPathFromConfigFile2(string config_file_path);
 string	ReadConsoleParameter (string str_pattern, int argc, string argv[], bool is_flag_param = FALSE);
 bool    ParseFileParameter (string str_file_param, list<string> &file_list, int &output_bands_num, int **&pp_band_mapping);
 
