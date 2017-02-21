5e014107c9ef39ba0a4ddef395d31c92496dd9db
Michigan <mike@scanex.ru>
2017-02-21 19:40:44 +0300

    add copytiles folder back

---
+++ copytiles/stdafx.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define _FILE_OFFSET_BITS 64
+#include <stdio.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <fstream>
+#include <cstdlib.>
+#include <iostream>
+#include <regex>
+using namespace std;
+
+
+#include "../TilingToolsLib/FileSystemFuncs.h"
+#include "../TilingToolsLib/TilingFuncs.h"
+#include "../TilingToolsLib/ConsoleUtils.h"
