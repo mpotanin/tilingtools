f840920eb42e4117aae10f203b8697adbe9624c6
Michigan <mike@scanex.ru>
2017-02-21 17:44:09 +0300

    add back imagetiling folder

---
+++ imagetiling/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define _FILE_OFFSET_BITS 64
+#include <stdio.h>
+#include <tchar.h>
+#include <string>
+#include <math.h>
+#include <assert.h>
+#include <fstream>
+#include <cstdlib.>
+#include <iostream>
+#include <regex>
+using namespace std;
+
+
+#include "../tilingtoolslib/filesystemfuncs.h"
+#include "../tilingtoolslib/tilingfuncs.h"
+#include "../tilingtoolslib/tilingparameters.h"
+#include "../tilingtoolslib/consoleutils.h"
+
+
